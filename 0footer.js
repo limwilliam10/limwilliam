f04cddd5235e20309a2dee94fca1f598aeabc12b
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2024-11-26 11:37:51 +0700

    header and footer js

---
+++ 0footer.js
@@ -0,0 +1,63 @@
+let appFooter = `
+    <nav>
+
+
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+      <title>William Lim's Portfolio</title>
+      <meta content="" name="description">
+      <meta content="" name="keywords">
+
+      <!-- Favicons -->
+      <link href="assets/img/profile-img2.png" rel="icon">
+      <link href="assets/img/profile-img2.png" rel="apple-touch-icon">
+
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+      <!-- Vendor CSS Files -->
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+      <!-- Template Main CSS File -->
+      <link href="assets/css/style.css" rel="stylesheet">
+
+    </head>
+    
+    <body>
+    
+      <!-- ======= Footer ======= -->
+      <footer id="footer">
+        <div class="container">
+          <div class="copyright">
+            &copy; Copyright <strong><span><a href="https://www.limwilliam.com/">WilliamLim</a></span></strong>
+          </div>
+          <div class="credits">
+            All rights reserved.
+          </div>
+        </div>
+      </footer><!-- End  Footer -->
+    
+    </body>
+    
+    </html>
+
+
+
+
+    
+    </nav>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ 0header.js
@@ -0,0 +1,80 @@
+let appHeader = `
+    <nav>
+
+
+
+    
+
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+      <title>William Lim's Portfolio</title>
+      <meta content="" name="description">
+      <meta content="" name="keywords">
+    
+      <!-- Favicons -->
+      <link href="assets/img/profile-img2.png" rel="icon">
+      <link href="assets/img/profile-img2.png" rel="apple-touch-icon">
+    
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+      <!-- Vendor CSS Files -->
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    
+      <!-- Template Main CSS File -->
+      <link href="assets/css/style.css" rel="stylesheet">
+    
+    </head>
+    
+    <body>
+    
+      <!-- ======= Header ======= -->
+      <header id="header">
+        <div class="d-flex flex-column">
+
+          <div class="profile">
+            <img src="assets/img/profile-img.png" alt="" class="img-fluid rounded-circle">
+            <h1 class="text-light"><a href="index.html">William Lim</a></h1>
+            <div class="social-links mt-3 text-center">
+              <a href="https://www.facebook.com/sherringford/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/limwilliam10/" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://wa.me/+6281218035252" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+              <a href="https://www.linkedin.com/in/lim-william/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <a href="mailto:limwilliam.id@gmail.com" class="google"><i class="bx bxl-google"></i></a>
+            </div>
+          </div>
+
+          <nav id="navbar" class="nav-menu navbar">
+            <ul>
+              <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+              <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+              <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+              <!--<li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
+              <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>-->
+              <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+            </ul>
+          </nav><!-- .nav-menu -->
+        </div>
+      </header><!-- End Header -->
+    
+    </body>
+    
+    </html>
+
+
+
+
+
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
